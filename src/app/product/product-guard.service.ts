8d2548615688e31a1615c467d126c94e3775bb41
NazarKupr <KuprNazar@gmail.com>
2018-04-16 13:37:13 +0300

    First Commit

---
+++ src/app/product/product-guard.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, Router } from '@angular/router';
+
+@Injectable()
+export class ProductGuardService implements CanActivate{
+
+  constructor(private _router:Router) { }
+  canActivate(router : ActivatedRouteSnapshot) : boolean{
+    let id = +router.url[1].path;
+    if(isNaN(id) || id<1){
+      alert("Invalid product Id");
+      this._router.navigate(["/products"]);
+      return false;
+    }
+    return true;
+  }
+}
